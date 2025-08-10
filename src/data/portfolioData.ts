d31024ee720987ce1113bfa9125d593319f7b3dc
Rohan <shingaderohan96@gmail.com>
2025-08-10 18:43:00 +0530

    portfolioData added

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,302 @@
+export const portfolioData = {
+  hero: {
+    name: "ROHAN SHINGADE",
+    roles: [
+      "REACT JS DEVELOPER",
+      "FRONTEND DEVELOPER",
+      "WEB DEVELOPER",
+      "FREELANCER",
+    ],
+    backgroundImage: "https://project-assets-phi.vercel.app/assets/portfolio-images/bgPortfolio.jpg",
+    socialLinks: {
+      twitter: "https://x.com/Pseudo_tsx",
+      github: "https://github.com/Rohhaannn",
+      linkedin: "https://www.linkedin.com/in/rohan-s-a56210165/",
+      discord: "https://discord.gg/K2eE44WzUX",
+      behance: "https://www.behance.net/rohhaannn",
+    },
+  },
+
+  about: {
+    description:
+      "A Frontend Developer with 2.4 years of experience, specializing in scalable React.js applications and responsive UIs. I'm proficient in Next.js, Tailwind CSS, Redux, and REST APIs, with a strong focus on clean code and performance optimization. My expertise includes developing dynamic Single-Page Applications (SPAs) and responsive websites, and I have a proven ability to lead teams and meet project deadlines. I'm a quick and adaptable learner, always seeking to contribute to organizational growth in a challenging React Developer or Frontend Developer role.",
+    roles: [
+      "React Js Developer",
+      "Front-End Developer",
+      "Web Developer",
+      "Freelancer",
+    ],
+    image:
+      "https://project-assets-phi.vercel.app/assets/portfolio-images/cartoonDeveloper.png",
+  },
+
+  skills: {
+    Frontend: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "Next.js" },
+      { id: 3, name: "Redux" },
+      { id: 4, name: "Tailwind CSS" },
+      { id: 5, name: "Bootstrap" },
+    ],
+    Backend: [
+      { id: 1, name: "Node.js" },
+      { id: 2, name: "Express.js" },
+      { id: 4, name: "Postman" },
+      { id: 3, name: "Appwrite" },
+    ],
+    Languages: [
+      { id: 1, name: "C" },
+      { id: 2, name: "CPP" },
+      { id: 3, name: "Java" },
+      { id: 4, name: "Python" },
+      { id: 5, name: "JavaScript" },
+      { id: 6, name: "Typescript" },
+    ],
+    Databases: [
+      { id: 1, name: "MongoDB" },
+      { id: 2, name: "MySQL" },
+    ],
+    Tools: [
+      { id: 1, name: "Git" },
+      { id: 2, name: "Github" },
+      { id: 4, name: "Docker" },
+      { id: 5, name: "VS Code" },
+      { id: 6, name: "Babel" },
+      { id: 7, name: "Notion" },
+      { id: 8, name: "Bash" },
+      { id: 9, name: "Figma" },
+    ],
+    DeploymentPlatforms: [
+      { id: 1, name: "Vercel" },
+      { id: 2, name: "Netlify" },
+    ],
+    image:
+      "https://project-assets-phi.vercel.app/assets/portfolio-images/skillsCharacter.svg",
+  },
+
+  workExperience: [
+    {
+      id: "1",
+      year: {
+        start: "Feb 2025",
+        end: "July 2025",
+      },
+      company: "Engineer's Cradle",
+      role: "Frontend Developer",
+      location: "Pune, Maharashtra",
+      duration: "",
+      description: [
+        "Led a team of 3 developers, managing tasks, conducting daily stand-ups, and evaluating performance to meet project deadlines.",
+        "Developed and redesigned responsive web pages, including creating new landing pages from scratch using Next.js, Typescript, and Tailwind CSS.",
+        "Improved the admin panel's design and structure based on feedback, enhancing usability and visual consistency.",
+        "Wrote and implemented backend schemas and validation to ensure data integrity and system reliability.",
+        "Collaborated with UI/UX designers in Figma to translate wireframes into pixel-perfect, interactive user interfaces.",
+        "Developed and maintained a comprehensive Storybook component library, significantly streamlining UI development and ensuring design consistency.",
+        "Proactively resolved complex Git merge conflicts, ensuring smooth integration of features and maintaining code integrity across multiple developer contributions.",
+      ],
+      techStack: [
+        "Next.js",
+        "Typescript",
+        "React",
+        "React Query",
+        "Storybook",
+        "Tailwind CSS",
+        "Figma",
+        "Zod",
+        "Github",
+        "PNPM & BUN",
+      ],
+    },
+    {
+      id: "2",
+      year: {
+        start: "Nov 2022",
+        end: "Nov 2024",
+      },
+      company: "Freelancer",
+      role: "Frontend Developer",
+      location: "Pune, Maharashtra",
+      duration: "",
+      description: [
+        "Built and maintained dynamic Single-Page Applications (SPAs) using React.js, plus websites with HTML, CSS, and JavaScript.",
+        "Translated user needs into technical plans, working with UX/UI and graphic designers to create user-friendly interfaces.",
+        "Used Redux for state management and integrated RESTful and third-party APIs for smooth front-end/back-end communication.",
+        "Optimized web applications and components for better performance, responsiveness, and faster load times.",
+        "Implemented responsive design and ensured cross-browser compatibility for a consistent user experience on all devices.",
+        "Developed reusable components and libraries to speed up development, using Git for efficient code management.",
+        "Conducted user testing, gathered feedback, and provided ongoing support and maintenance for deployed applications.",
+        "Stayed updated on web and React development trends, focusing on security and data privacy.",
+      ],
+      techStack: [
+        "React.js",
+        "Javascript",
+        "Redux Toolkit",
+        "Tailwind CSS",
+        "Node",
+        "Express",
+        "Postman",
+        "MongoDB",
+        "Daisy UI",
+        "Github",
+      ],
+    },
+    {
+      id: "3",
+      year: {
+        start: "Mar 2021",
+        end: "Feb 2022",
+      },
+      company: "Infosys",
+      role: "Technical Process Executive",
+      location: "Pune, Maharashtra",
+      duration: "",
+      description: [
+        "Analyzed client requirements and conceptualized website design objectives.",
+        "Collaborated with interdisciplinary teams to identify and resolve process inefficiencies, ensuring seamless web development workflows.",
+        "Implemented interactive and functional web pages using HTML, CSS, and JavaScript.",
+        "Integrated SEO-friendly textual and multimedia content for optimal search visibility.",
+        "Performed comprehensive testing and debugging to ensure robust website functionality.",
+        "Communicated effectively with team members and provided timely project updates.",
+        "Engaged in continuous learning to stay current with design trends and technologies.",
+        "Provided client support and maintained adherence to design best practices for project excellence.",
+      ],
+      techStack: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "MySQL",
+        "MS Office",
+        "Service Now tool",
+        "VLookup Tool",
+      ],
+    },
+  ],
+
+  projects: {
+    major: [
+      {
+        id: "1",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/mooncart.png",
+        title: "MoonCart - E-Commerce Website",
+        desc: "Developed a dynamic and responsive e-commerce website using React, featuring modern design, user-friendly navigation, and secure payment integration.",
+        githubLink: "https://github.com/Rohhaannn/Mooncart",
+        liveLink: "https://mooncart.vercel.app/",
+      },
+      {
+        id: "2",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/allposts.png",
+        title: "Blog App",
+        desc: "A dynamic React app powered by Vite and Tailwind CSS, integrated with Appwrite backend, enabling users to seamlessly sign up, sign in, and post content.",
+        githubLink: "https://github.com/Rohhaannn/React-Blog-App/tree/main/12MegaBlog",
+        liveLink: "https://react-blog-app-iota.vercel.app/",
+      },
+      {
+        id: "3",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/admin-page.png",
+        title: "School-Dashboard",
+        desc: "A comprehensive school management dashboard for students, teachers, parents, and admins.",
+        githubLink: "https://github.com/Rohhaannn/school-dashboard",
+        liveLink: "https://school-dashboard-ui.vercel.app/",
+      },
+    ],
+    mini: [
+      {
+        id: "4",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/virtualR.png",
+        title: "VirtualR Landing Page",
+        desc: "A modern virtual reality landing page built with React.js and Tailwind CSS.",
+        githubLink: "https://github.com/Rohhaannn/VirtualR-Landing-Page",
+        liveLink: "https://virtual-r-landing-page-eta.vercel.app/",
+      },
+      {
+        id: "5",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/githubViewer.png",
+        title: "Github Profile Viewer",
+        desc: "Built a GitHub profile viewer app to search and explore GitHub profiles.",
+        githubLink: "https://github.com/Rohhaannn/github-profile-viewer",
+        liveLink: "https://githubfinderr.vercel.app/",
+      },
+      {
+        id: "6",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/todoRedux.png",
+        title: "Todo With Redux-Toolkit",
+        desc: "A Todo app built with React and Redux Toolkit for state management.",
+        githubLink: "https://github.com/Rohhaannn/Todo--redux-toolkit",
+        liveLink: "https://todo-redux-toolkit-chi.vercel.app/",
+      },
+      {
+        id: "7",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/Todo.png",
+        title: "Todo with Context & local",
+        desc: "A Todo list app using Context API and local storage.",
+        githubLink:
+          "https://github.com/Rohhaannn/react-deployed-projects/tree/main/10todoContextLocal",
+        liveLink: "https://todo-contextlocal.vercel.app/",
+      },
+      {
+        id: "8",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/pswrdGen.png",
+        title: "Password Generator",
+        desc: "Password generator app using React and Tailwind CSS.",
+        githubLink:
+          "https://github.com/Rohhaannn/react-deployed-projects/tree/main/05passwordGenerator",
+        liveLink: "https://password-generator-azure-xi.vercel.app/",
+      },
+      {
+        id: "9",
+        imgLink:
+          "https://project-assets-phi.vercel.app/assets/portfolio-images/currency.png",
+        title: "Currency Convertor",
+        desc: "A currency converter app using React and Tailwind CSS.",
+        githubLink:
+          "https://github.com/Rohhaannn/react-deployed-projects/tree/main/06currencyConvertor",
+        liveLink: "https://currency-converter-inky-iota.vercel.app/",
+      },
+    ],
+  },
+
+  contact: {
+    image:
+      "https://project-assets-phi.vercel.app/assets/portfolio-images/contact.png",
+    formEndpoint: "https://getform.io/f/bmdpmnna",
+  },
+
+  footer: {
+    name: "Rohan Shingade",
+    socialLinks: [
+      {
+        id: "twitter-link",
+        type: "twitter",
+        link: "https://twitter.com/i_m_rohaannn",
+      },
+      {
+        id: "github-link",
+        type: "github",
+        link: "https://github.com/Rohhaannn",
+      },
+      {
+        id: "linkedin-link",
+        type: "linkedin",
+        link: "https://www.linkedin.com/in/rohan-s-a56210165/",
+      },
+      {
+        id: "discord-link",
+        type: "discord",
+        link: "https://discord.gg/K2eE44WzUX",
+      },
+      {
+        id: "behance-link",
+        type: "behance",
+        link: "https://www.behance.net/rohhaannn",
+      },
+    ],
+  },
+};
