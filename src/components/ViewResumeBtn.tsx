8270293975b1bb68e81a2141e6104dc30654b200
Rohan <shingaderohan96@gmail.com>
2025-08-13 22:31:02 +0530

    drive resume link updated

--- src/components/ViewResumeBtn.tsx
+++ src/components/ViewResumeBtn.tsx
@@ -7,7 +7,7 @@ import { FaFilePdf } from 'react-icons/fa';
 const ViewResumeBtn: FC = () => {
 
   const openResume = () => {
-    const resumeLink = 'https://drive.google.com/file/d/1TxAFnFXKVBnGQZXfpxgOyUoSJlIrtmlx/view?usp=sharing';
+    const resumeLink = 'https://drive.google.com/file/d/1q0KyAzilONrRk1USRIQXhXJlZrYbgkNi/view?usp=sharing';
     window.open(resumeLink, '_blank');
   };
 
