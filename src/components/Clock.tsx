802f075d16f07b4e89e82ebaa85651624addc3db
Rohan <shingaderohan96@gmail.com>
2025-08-22 22:23:47 +0530

    time & location added along with Navbar component

---
+++ src/components/Clock.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { ImClock } from "react-icons/im";
+
+const Clock = () => {
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  const formatTime = (date: Date) => {
+    let hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12;
+
+    const formattedHours = hours.toString().padStart(2, '0');
+
+    return `${formattedHours}:${minutes}:${seconds} ${ampm}`;
+  };
+
+  return (
+    <div className="navbar-clock flex flex-row justify-center items-center gap-2 pb-0.5">
+      <ImClock size={18}/> {formatTime(time)}
+    </div>
+  );
+};
+
+export default Clock;
---
+++ src/components/CountryCity.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaLocationDot } from "react-icons/fa6";
+
+const CountryCity = () => {
+  return (
+    <div className='flex flex-row justify-center items-center gap-2 pb-0.5'>
+      <FaLocationDot size={18}/> India/Pune
+    </div>
+  )
+}
+
+export default CountryCity
