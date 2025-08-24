0de3eae8118f08dd814397454defce00a5757686
Rohan <shingaderohan96@gmail.com>
2025-08-24 12:30:46 +0530

    ThemeContext added for switching themes

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,52 @@
+
+"use client"
+
+import React, {createContext, useState, useContext, ReactNode } from 'react'
+
+interface ThemeContextType {
+  darkMode: boolean;
+  toggleDark: () => void;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeContext = createContext <ThemeContextType | undefined> (undefined);
+
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const toggleDark = () => {
+    setDarkMode (prevMode => {
+      const newMode = !prevMode;
+
+      if (newMode) {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+      return newMode;
+    });
+  };
+
+  const contextValue = React.useMemo(() => ({ darkMode, toggleDark }), [darkMode, toggleDark]);
+
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      { children }
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+
