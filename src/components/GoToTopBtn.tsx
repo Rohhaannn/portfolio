db2d110f276afe51056e88e9a6259d83a942c9be
Rohan <shingaderohan96@gmail.com>
2025-08-24 19:07:25 +0530

    bg color updated

--- src/components/GoToTopBtn.tsx
+++ src/components/GoToTopBtn.tsx
@@ -33,7 +33,7 @@ const GoToTopBtn: FC = () => {
     <>
       {isVisible && (
         <button
-          className=" max-w-[1280px] mx-auto fixed bottom-4 right-3 bg-blue-800 backdrop-blur-2xl text-white hover:text-black px-2 lg:px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:bg-slate-300 hover:border hover:border-black z-[999]"
+          className=" max-w-[1280px] mx-auto fixed bottom-4 right-3 bg-blue-600 backdrop-blur-2xl text-white hover:text-black px-2 lg:px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:bg-slate-300 hover:border hover:border-black z-[999]"
           onClick={scrollToTop}
         >
           <FaAngleUp size={20} className="size-4 lg:size-6" />
