d198ca27197df4a5244dc6c030d6f9b6da58e9f9
Rohan <shingaderohan96@gmail.com>
2025-08-31 23:35:05 +0530

    font style added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins, Ubuntu} from "next/font/google";
 import "./globals.css";
 import GoToTopBtn from "@/components/GoToTopBtn";
 import ViewResumeBtn from "@/components/ViewResumeBtn";
@@ -7,7 +7,6 @@ import ScrollProgressBar from "./ScrollProgressBar";
 import Navbar from "@/components/Navbar";
 import Footer from '@/components/Footer'
 import  Providers  from '@/components/Providers';
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,6 +17,19 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
+
+const ubuntu = Ubuntu({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-ubuntu',
+});
+
 
 export const metadata: Metadata = {
   title: "Rohan - Frontend Developer",
@@ -31,7 +43,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen w-full overflow-x-hidden`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${ubuntu.variable} antialiased min-h-screen w-full overflow-x-hidden`}>
         <Providers>
           <ScrollProgressBar />
           <Navbar />
