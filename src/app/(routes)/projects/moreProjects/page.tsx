fabb7a4bf9ceb63df22eb5eb52db9edacae8688c
Rohan <shingaderohan96@gmail.com>
2025-09-04 15:37:51 +0530

    moreProjects component rendered

---
+++ src/app/(routes)/projects/moreProjects/page.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import MoreProjects from '@/components/projects/MoreProjects'
+
+const moreProjects = () => {
+  return (
+    <div>
+      <MoreProjects/>
+    </div>
+  )
+}
+
+export default moreProjects
