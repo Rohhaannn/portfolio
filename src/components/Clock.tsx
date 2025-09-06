6db86266744ecac78e25075cd8ad15dbf576e37e
Rohan <shingaderohan96@gmail.com>
2025-09-06 22:08:07 +0530

    time rendered conditionally

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { ImClock } from "react-icons/im";
 
 const Clock = () => {
-
-  const [time, setTime] = useState(new Date());
+  
+  const [time, setTime] = useState<Date | null>(null);
 
   useEffect(() => {
+
     const timerId = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
+
     return () => clearInterval(timerId);
   }, []);
-
   const formatTime = (date: Date) => {
     let hours = date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, '0');
@@ -28,7 +29,9 @@ const Clock = () => {
 
   return (
     <div className="navbar-clock flex flex-row justify-center items-center gap-2 py-0.5">
-      <ImClock size={18} className='text-sky-400'/> {formatTime(time)}
+      <ImClock size={18} className='text-sky-400'/>
+
+      {time ? formatTime(time) : 'Loading...'}
     </div>
   );
 };
